ddc34772fc33875dc9118692bd8a789a68828e01
Henrik Söderlind <henrik.soderlind@invono.se>
2022-10-04 20:26:21 +0200

    ...

--- src/Api/Action/GetAttributeDefinitions.php
+++ src/Api/Action/GetAttributeDefinitions.php
@@ -4,11 +4,20 @@ namespace Hsoderlind\Tradera\Api\Action;
 
 use Hsoderlind\Tradera\Api\Service\TraderaPublicService;
 use Hsoderlind\Tradera\Api\Provider\GetAttributeDefinitionsResult;
+use stdClass;
 
 class GetAttributeDefinitions
 {
+    /** @var int */
+    protected $categoryId;
+
+    public function setCategoryId(int $categoryId): void
+    {
+        $this->categoryId = $categoryId;
+    }
+
     public function dispatch(TraderaPublicService $client): GetAttributeDefinitionsResult
     {
-        return new GetAttributeDefinitionsResult($client->GetAttributeDefinitions());
+        return new GetAttributeDefinitionsResult($client->GetAttributeDefinitions($this));
     }
 }
